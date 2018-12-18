83f9bc9b6dc9d807a0c3f95f90c8b72f73fdbd6f
stefanvlahov <stefan.vlahov5@gmail.com>
2018-12-17 22:13:39 -0600

    Add header and recipe module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ShoppingListComponent } from './shoppinglist/shopping-list/shopping-list.component';
-import {ShoppingListEditComponent} from './shoppinglist/shopping-list/shopping-edit/shopping-list-edit.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
@@ -15,7 +15,7 @@ import { RecipesComponent } from './recipes/recipes.component';
     AppComponent,
     HeaderComponent,
     ShoppingListComponent,
-    ShoppingListEditComponent,
+    ShoppingEditComponent,
     RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
